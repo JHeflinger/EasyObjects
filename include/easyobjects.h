1c74e00dbaf88c944a489b8d0fbd17dcfea5952c
Jason Heflinger <jasonheflinger@gmail.com>
2024-05-20 23:05:50 -0400

    added bool definition

--- include/easyobjects.h
+++ include/easyobjects.h
@@ -5,6 +5,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#ifndef
+
+#ifndef BOOL
+#define BOOL int 
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif 
+
+#ifndef FALSE
+#define FALSE 0
+#endif
 
 // dynamic structs
 #define DECLARE_ARRLIST(T) \
